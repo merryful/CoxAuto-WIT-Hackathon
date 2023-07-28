0bd479f0e9b3044392f946bcabd7007ac4b1b06b
Merry Nguyen <87429372+merryful@users.noreply.github.com>
2023-07-28 14:24:27 -0400

    added project update

--- main.js
+++ main.js
@@ -19,13 +19,16 @@ async function getMap() {
     const data = await response.json();
     const { latitude, longitude } = data;
 
-   // Set multiple marker locations with tooltips
-   const markerLocations = [
-    { coords: [latitude, longitude], name: 'Current ISS location' },
-    { coords: [33.080803, -83.229658], name: '(Academic) Georgia College & State University', address: '231 W. Hancock St, Milledgeville, GA 31061', phone:'' },
-    { coords: [51.5074, -0.1278], name: 'London' },
-    { coords: [35.6895, 139.6917], name: 'Tokyo' },
-    // Add more locations as needed
+    const markerLocations = [
+        { coords: [latitude, longitude], name: 'Current ISS location' },
+        { coords: [33.080803, -83.229658], name: 'Georgia College & State University', address: '231 W. Hancock St, Milledgeville, GA 31061', phone:'' },
+        { coords: [33.47835600209376, -82.47668220958346], name: 'Thomson-McDuffie Middle School', address: '1191 White Oak Road', phone:'Phone: (706) 986-4300'},
+        { coords: [33.33700908607529, -84.10795056947856], name: 'Locust Grove Public Library', address: '115 Martin Luther King jr Blvd Locust Grove, Georgia 30248', phone:'+16784325353'},
+        { coords: [31.6723482626435, -83.2427116870719], name: 'Fitzgerald High School', address: '553 Ocilla Highway Fitzgerald, Georgia 31750', phone:'+12294095530' },
+        { coords: [33.86697282421514, -83.41368179442294], name: 'Oconee Public Library', address: '1080 Experiment Station Rd Watkinsville, Georgia 30677', phone:'+17067693950' },
+        { coords: [34.06027392652711, -84.22738473566892], name: 'Infinity Learning Educational Services, LLC', address: '142 Prospect Place Alpharetta, Georgia 30005', phone:'(770) 322-4185' },
+        { coords: [34.09997050150877, -84.33580084659154], name: 'Milton High School', address: '13025 Birmingham Hwy, Milton, GA 30004', phone:'+14702547000' },
+        { coords: [33.88639627062406, -84.1191334473696], name: 'Trickum Middle School', address: '130 Killian Hill Rd SW, Lilburn, GA 30047', phone:'+17709212705' }
     ];
 
     // Loop through the marker locations and add markers with tooltips to the map
